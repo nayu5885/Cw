aa7a4ffc4513e54f16d4f36ad0e04e253de8c338
lifeistech <happymmw5885@gmail.com>
2020-12-05 15:46:53 +0900

    Initial commit

---
+++ app/src/main/java/app/asahi/nayu/cw/MainActivity.kt
@@ -0,0 +1,31 @@
+package app.asahi.nayu.cw
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    var count:Int=0
+    var count2:Int=0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button: Button =findViewById(R.id.button)
+        val button2:Button=findViewById(R.id.button2)
+        textView.text="0"
+
+        button.setOnClickListener {
+            count++
+            textView.text=count.toString()
+        }
+
+        button2.setOnClickListener {
+            count+= 10
+            textView.text=count.toString()
+        }
+    }
+}
